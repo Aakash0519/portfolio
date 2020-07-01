dfd49a7267cc38f6ecbe4179aa989227c9bd5cb1
aakash0519 <19uec168@lnmiit.ac.in>
2020-07-01 21:10:46 +0530

    Updates

---
+++ precache-manifest.a95303f620ad2ba0069e64978bbd34a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fb417692ea8a20ef1e789ce49866dd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "77a2ed65838dc9a22b6c",
+    "url": "/portfolio/static/css/main.2df0f944.chunk.css"
+  },
+  {
+    "revision": "3ae562bc8ee4fb777c26",
+    "url": "/portfolio/static/js/2.fc92101c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.fc92101c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a2ed65838dc9a22b6c",
+    "url": "/portfolio/static/js/main.52aa6524.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "27cc64b44d5f56548852a2fb098829fb",
+    "url": "/portfolio/static/media/Stars.27cc64b4.png"
+  },
+  {
+    "revision": "b0ec457f517f29a35a8cf88006b502cc",
+    "url": "/portfolio/static/media/envelope.b0ec457f.svg"
+  },
+  {
+    "revision": "cbb8b0fb31624765bb1e6ae103ceece3",
+    "url": "/portfolio/static/media/pic.cbb8b0fb.png"
+  }
+]);
